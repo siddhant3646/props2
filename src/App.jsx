340d50610a334e313252c37c41c68f833aec7edf
Siddhant Singh <siddhant3646@gmail.com>
2024-03-11 23:29:44 +0530

    Adding Basic elements.

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export function App()
+{
+    return(
+        <>
+        <p> Hi Siddhant</p>
+        </>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+const rootdiv = document.getElementById("root");
+const reactroot = ReactDOM.createRoot(rootdiv);
+reactroot.render(<App />);
